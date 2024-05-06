8993b3f01966bb623f8e8d1d14e9793bc9b552f9
Waleed Tariq <Waleedtariq47@yahoo.com>
2024-05-06 09:18:14 +0300

    Add the tab switching logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-import './style.css'
-import welcome, {workingHours} from "./welcome-page"
+import './style.css';
+import home from "./welcome-page";
+import menu from './menu';
+import about from './about';
 
-welcome()
+const homeButton = document.querySelector("#home");
+homeButton.addEventListener("click", () => displayPage(home));
 
-workingHours();
+const menuButton = document.getElementById("menu");
+menuButton.addEventListener("click", () => displayPage(menu));
+
+const aboutButton = document.getElementById("about");
+aboutButton.addEventListener("click", () => displayPage(about));
+
+
+function displayPage(page){
+    document.getElementById("content").innerHTML = ""// to remove the previously loaded page
+    page();// to display the new page
+}
